5386ede5403df8d3397d412e5daae7d00b4b4297
cunxinshuihua <cunxinshuihua@gmail.com>
2017-05-12 20:40:52 +0800

    调整常量位置

--- src/util/ConstDefinition.java
+++ src/util/ConstDefinition.java
@@ -1,25 +1,4 @@
 package util;
 
 public final class ConstDefinition {
-	public final static String[] OP = {
-		"ADD",
-		"SUB",
-		"MUL",
-		"DIV",
-		"LOAD",
-		"STOR"
-	};
-	
-	public final static String[] REGS = {
-		"F0",
-		"F1",
-		"F2",
-		"F3",
-		"F4",
-		"F5",
-		"F6",
-		"F7",
-		"F8",
-		"F9",
-	};
 }
