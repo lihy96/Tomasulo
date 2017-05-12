354f36b133d716f8fe7c7397e828cf90b4b7d94c
cunxinshuihua <cunxinshuihua@gmail.com>
2017-05-12 21:12:05 +0800

    指令类的完善

--- src/util/Instr.java
+++ src/util/Instr.java
@@ -1,5 +1,7 @@
 package util;
 
+import kernel.ReserveStackEntry;
+
 public class Instr {
 
 	public enum OP {
@@ -23,4 +25,16 @@ public class Instr {
 		F8,
 		F9
 	};
+	
+	public OP op;	// 要对源操作数进行的操作
+	
+	/* 源寄存器地址，
+	 * Load，Store指令默认使用R1: LOAD des, imm(src1)
+	 * 其他指令格式：ADD des, src1, src2
+	 */
+	public REG src1, src2;	
+	public REG des;	// 目的地址寄存器
+	public int imm;	// Load,Store指令中的立即数
+	
+	
 }
