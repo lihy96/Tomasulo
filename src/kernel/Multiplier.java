f6458a362a49f49b532165eb5bcd9208f2c8ba97
张鹿颂 <zhanglusongg@gmail.com>
2017-05-12 21:24:59 +0800

    完成加法器，乘法器

--- src/kernel/Multiplier.java
+++ src/kernel/Multiplier.java
@@ -5,19 +5,19 @@ import util.Instr;
 import util.Instr.OP;
 import util.Instr.REG;
 
-public class Adder {
+public class Multiplier {
 	private int time;
 	private Instr instr;
 	private FloatPointRegister fp;
-	Adder(FloatPointRegister fp){
+	Multiplier(FloatPointRegister fp){
 		this.fp = fp;
 	}
 	void run(Instr instr) {
 		this.instr = instr;
-		if (instr.op == OP.ADD) {
+		if (instr.op == OP.MUL) {
 			time = ConstDefinition.OP_TIME[0];
 		}
-		else if (instr.op == OP.SUB) {
+		else if (instr.op == OP.DIV) {
 			time = ConstDefinition.OP_TIME[1];
 		}
 		else {
@@ -28,11 +28,11 @@ public class Adder {
 		time--;
 		if (time == 0) {
 			double ans;
-			if (instr.op == OP.ADD) {
-				ans = fp.get(instr.src1.ordinal()) + fp.get(instr.src2.ordinal());
+			if (instr.op == OP.MUL) {
+				ans = fp.get(instr.src1.ordinal()) * fp.get(instr.src2.ordinal());
 			}
 			else {
-				ans = fp.get(instr.src1.ordinal()) - fp.get(instr.src2.ordinal());
+				ans = fp.get(instr.src1.ordinal()) / fp.get(instr.src2.ordinal());
 			}
 			fp.set(instr.des.ordinal(), ans);
 		}
