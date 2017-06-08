1fc942dd659e30c2fb2ffb250eb0437815d6d7ea
cunxinshuihua <cunxinshuihua@gmail.com>
2017-06-08 14:06:42 +0800

    修改资源文件的获取方式

--- src/gui/MyImage.java
+++ src/gui/MyImage.java
@@ -3,6 +3,7 @@ package gui;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.net.URL;
 import java.awt.Image;
 import javax.imageio.ImageIO;
 
@@ -25,24 +26,55 @@ public class MyImage {
 	public static int NEW_HEIGHT = 36;
 	
 	public static void init_img(){
+		MyImage myImage = new MyImage();
+//		try {
+			img_next = myImage.readImage("/images/next.png").getScaledInstance( NEW_WIDTH, NEW_HEIGHT,  java.awt.Image.SCALE_SMOOTH );
+			img_clock = myImage.readImage("/images/clock.png").getScaledInstance( NEW_WIDTH, NEW_HEIGHT,  java.awt.Image.SCALE_SMOOTH );
+			img_mem = myImage.readImage("/images/mem.png").getScaledInstance( NEW_WIDTH, NEW_HEIGHT,  java.awt.Image.SCALE_SMOOTH );
+			img_reg = myImage.readImage("/images/reg.png").getScaledInstance( NEW_WIDTH, NEW_HEIGHT,  java.awt.Image.SCALE_SMOOTH );
+			img_stop = myImage.readImage("/images/stop.png").getScaledInstance( NEW_WIDTH, NEW_HEIGHT,  java.awt.Image.SCALE_SMOOTH );
+			
+			img_export_mem = myImage.readImage("/images/export_mem.png").getScaledInstance( NEW_WIDTH, NEW_HEIGHT,  java.awt.Image.SCALE_SMOOTH ).getScaledInstance( NEW_WIDTH, NEW_HEIGHT,  java.awt.Image.SCALE_SMOOTH );
+			img_export_reg = myImage.readImage("/images/export_reg.png").getScaledInstance( NEW_WIDTH, NEW_HEIGHT,  java.awt.Image.SCALE_SMOOTH ).getScaledInstance( NEW_WIDTH, NEW_HEIGHT,  java.awt.Image.SCALE_SMOOTH );
+			img_a = myImage.readImage("/images/a.png").getScaledInstance( NEW_WIDTH, NEW_HEIGHT,  java.awt.Image.SCALE_SMOOTH );
+			img_add = myImage.readImage("/images/add.png").getScaledInstance( 25, 25,  java.awt.Image.SCALE_SMOOTH );
+			img_sub = myImage.readImage("/images/sub.png").getScaledInstance( NEW_WIDTH, NEW_HEIGHT,  java.awt.Image.SCALE_SMOOTH );
+			img_pc = myImage.readImage("/images/pc.png").getScaledInstance( 40, 40,  java.awt.Image.SCALE_SMOOTH );
+			img_load = myImage.readImage("/images/load.png").getScaledInstance( 40, 40,  java.awt.Image.SCALE_SMOOTH );
+
+//			img_next = ImageIO.read(new File("static/images/next.png")).getScaledInstance( NEW_WIDTH, NEW_HEIGHT,  java.awt.Image.SCALE_SMOOTH );
+//			img_clock = ImageIO.read(new File("static/images/clock.png")).getScaledInstance( NEW_WIDTH, NEW_HEIGHT,  java.awt.Image.SCALE_SMOOTH );
+//			img_mem = ImageIO.read(new File("static/images/mem.png")).getScaledInstance( NEW_WIDTH, NEW_HEIGHT,  java.awt.Image.SCALE_SMOOTH );
+//			img_reg = ImageIO.read(new File("static/images/reg.png")).getScaledInstance( NEW_WIDTH, NEW_HEIGHT,  java.awt.Image.SCALE_SMOOTH );;
+//			img_stop = ImageIO.read(new File("static/images/stop.png")).getScaledInstance( NEW_WIDTH, NEW_HEIGHT,  java.awt.Image.SCALE_SMOOTH );
+//			
+//			img_export_mem = ImageIO.read(new File("static/images/export_mem.png")).getScaledInstance( NEW_WIDTH, NEW_HEIGHT,  java.awt.Image.SCALE_SMOOTH );; 
+//			img_export_reg = ImageIO.read(new File("static/images/export_reg.png")).getScaledInstance( NEW_WIDTH, NEW_HEIGHT,  java.awt.Image.SCALE_SMOOTH );;
+//			img_a = ImageIO.read(new File("static/images/a.png")).getScaledInstance( NEW_WIDTH, NEW_HEIGHT,  java.awt.Image.SCALE_SMOOTH );;
+//			img_add = ImageIO.read(new File("static/images/add.png")).getScaledInstance( 25, 25,  java.awt.Image.SCALE_SMOOTH );;
+//			img_sub = ImageIO.read(new File("static/images/sub.png")).getScaledInstance( NEW_WIDTH, NEW_HEIGHT,  java.awt.Image.SCALE_SMOOTH );;
+//			img_pc = ImageIO.read(new File("static/images/pc.png")).getScaledInstance( 40, 40,  java.awt.Image.SCALE_SMOOTH );;
+//			img_load = ImageIO.read(new File("static/images/load.png")).getScaledInstance( 40, 40,  java.awt.Image.SCALE_SMOOTH );;
+			
+//		} catch (IOException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+	}
+	
+	public Image readImage(String path) {
+		Image image = null;
 		try {
-			img_next = ImageIO.read(new File("static/images/next.png")).getScaledInstance( NEW_WIDTH, NEW_HEIGHT,  java.awt.Image.SCALE_SMOOTH );
-			img_clock = ImageIO.read(new File("static/images/clock.png")).getScaledInstance( NEW_WIDTH, NEW_HEIGHT,  java.awt.Image.SCALE_SMOOTH );
-			img_mem = ImageIO.read(new File("static/images/mem.png")).getScaledInstance( NEW_WIDTH, NEW_HEIGHT,  java.awt.Image.SCALE_SMOOTH );
-			img_reg = ImageIO.read(new File("static/images/reg.png")).getScaledInstance( NEW_WIDTH, NEW_HEIGHT,  java.awt.Image.SCALE_SMOOTH );;
-			img_stop = ImageIO.read(new File("static/images/stop.png")).getScaledInstance( NEW_WIDTH, NEW_HEIGHT,  java.awt.Image.SCALE_SMOOTH );
-			
-			img_export_mem = ImageIO.read(new File("static/images/export_mem.png")).getScaledInstance( NEW_WIDTH, NEW_HEIGHT,  java.awt.Image.SCALE_SMOOTH );; 
-			img_export_reg = ImageIO.read(new File("static/images/export_reg.png")).getScaledInstance( NEW_WIDTH, NEW_HEIGHT,  java.awt.Image.SCALE_SMOOTH );;
-			img_a = ImageIO.read(new File("static/images/a.png")).getScaledInstance( NEW_WIDTH, NEW_HEIGHT,  java.awt.Image.SCALE_SMOOTH );;
-			img_add = ImageIO.read(new File("static/images/add.png")).getScaledInstance( 25, 25,  java.awt.Image.SCALE_SMOOTH );;
-			img_sub = ImageIO.read(new File("static/images/sub.png")).getScaledInstance( NEW_WIDTH, NEW_HEIGHT,  java.awt.Image.SCALE_SMOOTH );;
-			img_pc = ImageIO.read(new File("static/images/pc.png")).getScaledInstance( 40, 40,  java.awt.Image.SCALE_SMOOTH );;
-			img_load = ImageIO.read(new File("static/images/load.png")).getScaledInstance( 40, 40,  java.awt.Image.SCALE_SMOOTH );;
-			
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
+//			System.out.println(path);
+			URL url = this.getClass().getResource(path);
+//			System.out.println(url);
+			image = ImageIO.read(url);
+//			.getScaledInstance( NEW_WIDTH, NEW_HEIGHT,  java.awt.Image.SCALE_SMOOTH );
+		}
+		catch (Exception e) {
+//			System.out.println("> Error at load image path : " + path);
 			e.printStackTrace();
 		}
+		return image;
 	}
 }
